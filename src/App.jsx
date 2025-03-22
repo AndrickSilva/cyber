a35bfd2da497f912f8ddb322046b6c8b53ee9006
avation-asilva <asilva@avation.com>
2025-03-22 13:22:06 +0530

    add About and Footer components; implement layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from "./components/Home";
 import Services from "./components/Services";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 function App() {
   return (
     <>
@@ -17,6 +18,7 @@ function App() {
         <About />
         <Contact />
       </div>
+      <Footer />
     </>
   );
 }
