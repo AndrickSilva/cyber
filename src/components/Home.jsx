4719c069c9e34c0c043547b83fb380f0a5ac8473
avation-asilva <asilva@avation.com>
2025-03-24 17:59:21 +0530

    Responsive home and navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,19 +4,19 @@ import { MoveRight } from "lucide-react";
 const Home = () => {
   return (
     <div id="home" className="min-h-[100vh] flex items-center justify-between gap-4 bg-secondary font-inter px-2">
-      <img src={security} className="h-[480px] w-1/2"></img>
-      <div className=" w-1/2">
+      <img src={security} className="h-[480px] w-1/2 hidden sm:block"></img>
+      <div className="p-6 sm:p-0 sm:w-1/2">
         <div className="max-w-[580px]">
-          <h1 className="text-5xl font-bold font-exo text-slate-800">
+          <h1 className="text-4xl sm:text-3xl lg:text-5xl font-bold font-exo text-slate-800">
             Rigorous Application Penetration Testing Services
           </h1>
-          <p className="text-lg mt-7 text-slate-700">
+          <p className="text-lg sm:text-sm lg:text-lg mt-7 text-slate-700">
             Adhere to the latest compliance standards and fast track time to
             launch. Our CREST-approved application penetration testing services
             detect and nullify every possible vulnerability.
           </p>
         </div>
-      <a href="#service" className="w-fit group bg-gradient-to-r bg-primary hover:bg-red-700 transition duration-300 ease-out cursor-pointer mt-7  text-white py-3 px-6 rounded-md flex items-center justify-center gap-1">
+      <a href="#service" className=" w-fit group bg-gradient-to-r bg-primary hover:bg-red-700 transition duration-300 ease-out cursor-pointer mt-7  text-white p-2 lg:py-3 lg:px-4 rounded-md flex items-center justify-center gap-1">
         <span>View Services</span>
         <MoveRight className="group-hover:translate-x-1.5 transition duration-300 ease-out"/>
         </a>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,48 @@
-import React from "react";
-// import reactLogo from '../assets/react.svg'
+import { X, Menu } from "lucide-react";
+import React, { useState } from "react";
+
 const Navbar = () => {
-return (
-        <nav className="bg-primary flex justify-between items-center py-2 px-12 drop-shadow-md font-semibold sticky top-0 z-50 font-exo">
-                {/* <img src={reactLogo} /> */}
-                <h1 className="font-bold text-2xl text-white">Cyber</h1>
-                <div className="flex gap-10 text-sm">
-                        <a href="#home" className="text-slate-200 hover:text-white">Home</a>
-                        <a href="#service" className="text-slate-200 hover:text-white">Service</a>
-                        <a href="#about" className="text-slate-200 hover:text-white">About</a>
-                </div>
-                <a href="#contact" className="text-white border-2 border-slate-200 px-6 py-1.5 rounded-sm hover:bg-white hover:text-primary transition duration-300 ease-out">Contact</a>
-        </nav>
-);
+  const [toggle, setToggle] = useState(false);
+
+  const handleMenuToggle = () => {
+    setToggle(!toggle);
+  };
+
+  return (
+    <nav className="bg-primary flex justify-between items-center py-3 px-6 md:px-12 drop-shadow-md font-semibold sticky top-0 z-50 font-exo">
+      {/* Logo */}
+      <h1 className="font-bold text-2xl text-white">Cyber</h1>
+      
+      {/* Menu Items - Hidden on small screens */}
+      <div className={`md:flex gap-10 text-sm hidden`}>
+        <a href="#home" className="text-slate-200 hover:text-white">Home</a>
+        <a href="#service" className="text-slate-200 hover:text-white">Service</a>
+        <a href="#about" className="text-slate-200 hover:text-white">About</a>
+      </div>
+      
+      {/* Contact Button */}
+      <a href="#contact" className="hidden md:block text-white border-2 border-slate-200 px-6 py-1.5 rounded-sm hover:bg-white hover:text-primary transition duration-300 ease-out">
+        Contact
+      </a>
+      
+      {/* Mobile Menu Toggle */}
+      <div className="md:hidden block cursor-pointer text-white" onClick={handleMenuToggle}>
+        {toggle ? <X /> : <Menu />}
+      </div>
+
+      {/* Mobile Menu - Visible on Toggle */}
+      {toggle && (
+        <div className="absolute top-13 h-screen left-0 w-full bg-primary flex flex-col items-center gap-16 py-6 md:hidden" onClick={handleMenuToggle}>
+          <a href="#home" className="text-slate-200 hover:text-white">Home</a>
+          <a href="#service" className="text-slate-200 hover:text-white">Service</a>
+          <a href="#about" className="text-slate-200 hover:text-white">About</a>
+          <a href="#contact" className="text-white border-2 border-slate-200 px-6 py-1.5 rounded-sm hover:bg-white hover:text-primary transition duration-300 ease-out">
+            Contact
+          </a>
+        </div>
+      )}
+    </nav>
+  );
 };
 
 export default Navbar;
